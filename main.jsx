7d1bd711123a66f56cd11b428411c5107c2cee39
Raissa Cunha <67392681+raissarang@users.noreply.github.com>
2026-05-18 15:20:46 +0200

    Add Main

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import PetNameMap from './PetNameMap'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PetNameMap />
+  </React.StrictMode>
+)
